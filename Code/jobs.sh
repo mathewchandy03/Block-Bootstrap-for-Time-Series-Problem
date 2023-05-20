ea57b8b75f4a1ea973a0f575a4a91834a14d9a93
Mathew Chandy <mathewchandy03@gmail.com>
2023-05-20 15:17:50 -0400

    New Data and Figures

---
+++ Code/jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=general
+#SBATCH --output=../out/ja_%A_%a.out
+#SBATCH --constraint="cpuonly" ## avoiding gpu (preventing waste of resources)
+#SBATCH --array=1-100%100
+#SBATCH --ntasks=1 ## OBS 1
+#SBATCH --cpus-per-task=1 ## OBS 2
+#SBATCH --mem-per-cpu=500M ## OBS 3
+
+module load r/4.2.2
+
+## avoiding implicit paralellism
+export OMP_NUM_THREADS=1
+
+Rscript jobs.R
