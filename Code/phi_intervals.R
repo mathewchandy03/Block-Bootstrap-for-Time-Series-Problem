f81f1a77f55b7887fc9d5d421d01271ab608525e
Mathew Chandy <mathewchandy03@gmail.com>
2023-06-07 18:20:35 -0400

    Faceted Interval Plots

--- Code/phi_intervals.R
+++ Code/phi_intervals.R
@@ -1,28 +1,32 @@
 source("functions.R")
+library(ggplot2)
+
 set.seed(1234)
-sim <- replicate(100, do1rep(800, 0.4, mystat, 10))
-new_sim <- sim[c(3, 6, 23, 24, 35, 36, 41, 42), ]
+sim <- replicate(50, do1rep(800, 0.4, mystat, 10))
+new_sim <- sim[c(3, 6, 23, 24, 35, 36, 47, 48), ]
 
-file_titles <- c("pct", "prop", "bc")
+file_titles <- c("pct", "bc", "prop")
+custom_titles <- c("Percentile", "Bias-Corrected", "Proposed")
 
-for (i in 1:3) {
-  pdf(paste('../Manuscript/figures/', file_titles[i], '.pdf', sep = ''))
-  lower_bound <- new_sim[(2 * i + 1), ]
-  upper_bound <- new_sim[(2 * i + 2), ]
-  point_estimate <- new_sim[1, ]
-  bts_mean <- new_sim[2, ]
-  
-  x_values <- 1:100
-  
-  plot(x_values, point_estimate, type = 'l', ylim = range(lower_bound, upper_bound),
-       xlab = 'Replication', ylab = 'Value', main = '', col = 'blue')
-  
-  lines(x_values, bts_mean, col = 'red')
-  # Add confidence intervals
-  lines(x_values, lower_bound, col = "gray")
-  lines(x_values, upper_bound, col = "gray")
-  abline(h = 0.4, lty = "dotted")
-  legend("topright", legend = c("Point Estimate", "Bootstrap Mean", "Interval Bounds"),
-         col = c('blue', 'red', 'gray'), lty = c(1, 1, 1), bty = "n")
-  dev.off()
-}
+# Create a data frame for plotting
+plot_data <- data.frame(replication = 1:50,
+                        point_estimate = new_sim[1, ],
+                        bts_mean = new_sim[2, ],
+                        lower_bound = c(new_sim[3, ], new_sim[5, ], new_sim[7, ]),
+                        upper_bound = c(new_sim[4, ], new_sim[6, ], new_sim[8, ]),
+                        file_title = rep(file_titles, each = 50),
+                        custom_title = rep(custom_titles, each = 50))
+
+# Create facet plot using ggplot2
+ggplot(plot_data, aes(x = replication, y = point_estimate)) +
+  geom_errorbar(aes(ymin = lower_bound, ymax = upper_bound), width = 0.5, color = 'gray') +
+  geom_line(color = 'blue') +
+  geom_line(aes(y = bts_mean), color = 'red') +
+  geom_hline(yintercept = 0.4, linetype = "dotted") +
+  facet_wrap(~ custom_title, ncol = 1) +
+  labs(x = 'Replication', y = 'Value') +
+  scale_color_manual(values = c('blue', 'red', 'gray')) +
+  scale_linetype_manual(values = c(1, 1, 1)) +
+  theme_minimal()
+
+ggsave('../Manuscript/figures/norm_phi_intervals.pdf')
