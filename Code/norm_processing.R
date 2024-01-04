0ef0106e66a9ec69b7f2ce906c0da0536fc22c28
Mathew Chandy <mathewchandy03@gmail.com>
2024-01-04 15:32:21 +0530

    Clopper Pearson Normal Mu

--- Code/norm_processing.R
+++ Code/norm_processing.R
@@ -11,10 +11,11 @@ grouped_matrices <- split(list_of_matrices, names(list_of_matrices))
 combined_matrices <- lapply(grouped_matrices, function(x) {
   Reduce(cbind, x)
 })
-df <- data.frame(phi = numeric(), n = numeric(), blksize = numeric(), 
+df <- data.frame(phi = numeric(), n = numeric(), blksize = numeric(),
                  CI = character(), 
                  target = character(), cov = numeric(),
-                 LB = numeric(), UB = numeric())
+                 LB = numeric(), UB = numeric(), CP_LB = numeric(), 
+                 CP_UB = numeric())
 for(i in names(combined_matrices)) {
   phi <- as.numeric(strsplit(i, '_')[[1]][1])
   n <- as.integer(strsplit(i, '_')[[1]][2])
@@ -26,9 +27,13 @@ for(i in names(combined_matrices)) {
   target <- rep(parameters, length(cov) / length(parameters))
   LB <- sapply(cov, function(i) prop.test(i*nrep, nrep)$conf.int[1])
   UB <- sapply(cov, function(i) prop.test(i*nrep, nrep)$conf.int[2])
+  CP_LB <- 
+    sapply(cov, function(i) exactci(i*nrep, nrep, .95)$conf.int[1])
+  CP_UB <- 
+    sapply(cov, function(i) exactci(i*nrep, nrep, .95)$conf.int[2])
   new_rows <- data.frame(phi = rep(phi, length(cov)), n = rep(n, length(cov)), 
                          blksize = rep(blksize, length(cov)), CI, target, cov, 
-                         LB, UB)
+                         LB, UB, CP_LB, CP_UB)
   df <- rbind(df, new_rows)
 }
 
--- Code/plots.R
+++ Code/plots.R
@@ -63,3 +63,33 @@ graph_bts(t, width, exp_sigma %>% filter(blksize == ceiling(2*n^(1/3))))
 t <- 'exp_phi_2'
 width <- .2
 graph_bts(t, width, exp_phi %>% filter(blksize == ceiling(2*n^(1/3))))
+
+# Exact Clopper-Pearson CI
+
+t <- 'norm_mu_1_cp'
+width <- .2
+data <- norm_mu %>% filter(blksize == ceiling(n^(1/3)))
+trans = 'identity'
+level = .95
+
+font_add_google("EB Garamond")
+#  windows()
+
+showtext_auto()
+
+data$CI <- factor(data$CI, levels = c('stdCI', 'stuCI', 'pctCI', 'bcCI', 'bcaCI', 'propCI'), 
+                  labels = c('Standard', 'Student\'s t', 'Percentile', 'BC', 'BCA', 'Recentered'))
+
+ggplot(data, aes(x = n, y = cov)) +
+  geom_hline(yintercept = level, linetype = 'dashed', color = 'orange') + 
+  geom_line(size = .2) +
+  geom_errorbar(aes(ymin=CP_LB, ymax = CP_UB), colour="black", width = width, size = .2) +
+  facet_grid(factor(CI) ~ factor(phi), scales = 'free') +
+  labs(x = 'Sample Size', y = 'Coverage Rate') +
+  scale_x_continuous(breaks = c(100, 200, 400, 800, 1600, 3200), trans='log2') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), text =
+          element_text(family = "EB Garamond", size = 10),
+        strip.text.y = element_text(angle = 270, hjust = 1)) +
+  scale_y_continuous(trans=trans)
+ggsave(paste('../Manuscript/figures/plot_', t, '.pdf', sep = ''), width = 10,
+       height = 5.8)
